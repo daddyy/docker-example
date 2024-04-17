d7914cdde1e0c4e10c5e5b0c5c59640f27fe8f31
Pavel Fiala <fiala@metrans.cz>
2024-04-17 07:17:36 +0200

    feat: add open telemetry

--- app/www/index.php
+++ app/www/index.php
@@ -1,4 +1,16 @@
 <?php
+use Monolog\Logger;
+use OpenTelemetry\API\Globals;
+use OpenTelemetry\Contrib\Logs\Monolog\Handler;
+use Psr\Log\LogLevel;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$loggerProvider = Globals::loggerProvider();
+$handler = new Handler(
+    $loggerProvider,
+    LogLevel::INFO
+);
+$monolog = new Logger('otel-php-monolog', [$handler]);
 
 echo "Hello world";
